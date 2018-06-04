2b541f82aa6921277f1919e80bdde21a6fc8081e
Javier Font <javifont@gmail.com>
2018-06-04 21:41:30 +0200

    Enemy AI

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] playerArray = GameObject.FindGameObjectsWithTag("Player");
+        GameObject target = playerArray[0];
+        Transform targetTransform = target.GetComponent(typeof(Transform)) as Transform;
+        transform.LookAt(targetTransform.position, transform.up);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+	}
+}
