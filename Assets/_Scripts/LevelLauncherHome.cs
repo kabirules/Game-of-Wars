6515892bccda2acbf3801a827f6819440637f631
kabirules <javifont@gmail.com>
2018-06-14 00:01:19 +0200

    Level changing Scripts

---
+++ Assets/_Scripts/LevelLauncherHome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelLauncherHome : MonoBehaviour {
+
+    //Make sure to attach these Buttons in the Inspector
+	public Button homeButton;
+
+    void Start()
+    {
+		Button btnHome = homeButton.GetComponent<Button>();
+		btnHome.onClick.AddListener(LevelChangeHome);
+    }
+
+    public void LevelChangeHome() {
+        Application.LoadLevel("Home");
+    }
+}
---
+++ Assets/_Scripts/LevelLauncherLevel1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelLauncherLevel1 : MonoBehaviour {
+
+    //Make sure to attach these Buttons in the Inspector
+    public Button level1Button;
+
+    void Start()
+    {
+        Button btnLevel1 = level1Button.GetComponent<Button>();
+        btnLevel1.onClick.AddListener(LevelChangeLevel1a);
+    }
+
+    public void LevelChangeLevel1a() {
+        Application.LoadLevel("Level1a");
+    }
+}
